a94d549cbeb92aee266f4c78ff7cb044a62bf05a
George Kaplan <george@georgekaplan.xyz>
2020-07-01 01:34:24 -0400

    reduce image size and add recurrence

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 FROM rust as build
 WORKDIR /app
-RUN rustup default nightly
+RUN rustup default nightly && rustup target add x86_64-unknown-linux-musl
 
 COPY . .
-RUN cargo build --release
+RUN cargo build --release --target=x86_64-unknown-linux-musl
 
-FROM ruby as prod
+FROM ruby:alpine as prod
 WORKDIR /poly
-RUN apt-get update && apt-get install bash jq -y
+RUN apk --update add bash jq
 RUN gem install twurl
 
 COPY ./post.sh .
-COPY --from=build /app/target/release/rand_poly /bin
+COPY --from=build /app/target/x86_64-unknown-linux-musl/release/rand_poly /bin
 CMD ["/bin/bash", "post.sh"]
--- post.sh
+++ post.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+set -e
 set -o pipefail
 
-TWEET=$(/bin/rand_poly)
+while true; do
+  TWEET=$(/bin/rand_poly)
 
-echo "tweet text will be $TWEET"
+  if [ -z "$TWEET" ]; then
+    exit 1
+  fi
 
-MEDIA_ID=$(
-    twurl -H upload.twitter.com -f tmp.png -F media -X POST /1.1/media/upload.json |\
-        jq ".media_id_string"
+  echo "tweet text will be $TWEET"
+
+  MEDIA_ID=$(
+      twurl -H upload.twitter.com -f tmp.png -F media -X POST /1.1/media/upload.json |\
+          jq ".media_id_string"
         )
 
-echo "media id is $MEDIA_ID"
-sleep 1
+  echo "media id is $MEDIA_ID"
+  sleep 1
 
-twurl -d "media_ids=${MEDIA_ID//\"/}&status=$TWEET" /1.1/statuses/update.json > /dev/null
+  twurl -d "media_ids=${MEDIA_ID//\"/}&status=$TWEET" /1.1/statuses/update.json > /dev/null
 
-echo "posted successfully"
+  echo "posted successfully"
+
+  sleep 5m
+done
